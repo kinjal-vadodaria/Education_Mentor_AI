3788b29e10e7373efc6ee36518b65bb604f28697
nikhilmallik <er.nikhilmallik@gmail.com>
2025-09-24 11:48:48 +0530

    Fix corrupted main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,17 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
+import App from './App.tsx'
+import './index.css'
+import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MantineProvider>
+      <Notifications />
+      <App />
+    </MantineProvider>
+  </React.StrictMode>,
+)
